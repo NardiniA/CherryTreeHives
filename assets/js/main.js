8406dbc6ee78821a1f4a0a00c37b13262eeb7a62
NardiniA <techteacher.pershore@gmail.com>
2022-01-12 16:18:59 +0000

    Purged CSS and added FAQ

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,34 @@ const mySwiper = new Swiper(".swiper-container", {
 const lightbox = GLightbox({
 	selector: ".folio-item__thumb-link",
 });
+
+/* FAQ
+ * ------------------------------------------------------ */
+const accordionItems = document.querySelectorAll('.questions__item')
+
+accordionItems.forEach((item) =>{
+	const accordionHeader = item.querySelector('.questions__header')
+
+	accordionHeader.addEventListener('click', () =>{
+		const openItem = document.querySelector('.accordion-open')
+
+		toggleItem(item)
+
+		// if(openItem && openItem!== item){
+		//     toggleItem(openItem)
+		// }
+	})
+})
+
+const toggleItem = (item) =>{
+	const accordionContent = item.querySelector('.questions__content')
+
+	if(item.classList.contains('accordion-open')){
+		accordionContent.removeAttribute('style')
+		item.classList.remove('accordion-open')
+	}else{
+		accordionContent.style.height = accordionContent.scrollHeight + 'px'
+		item.classList.add('accordion-open')
+	}
+
+}
